f18d14145a9415d766dc9d65a8218b5870dada64
Mohammad Razeenuddin Mehdi <razeenmehdi@Mohammads-MacBook-Pro.local>
2023-06-29 10:06:25 +0530

    Initial commit

---
+++ Coding/Cpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// testing function
+int harem() { return 4; }
+
+int main() {
+  cout << "How to form harem in middle-east?" << endl;
+  int wives = harem();
+
+  cout << "The number of wives Lone Cyborg has: " << wives << endl;
+  return 0;
+}
